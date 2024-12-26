7527b1b966fd612ecaa3a1980d04fed0724a6c66
rohithhh <rohithkalyankar@gmail.com>
2024-12-26 16:26:54 +0530

    Increasing readability

--- Arrays/src/prefixSum/SubarraysDivisibleByK.java
+++ Arrays/src/prefixSum/SubarraysDivisibleByK.java
@@ -9,13 +9,13 @@ public class SubarraysDivisibleByK {
 		System.out.println(subarraysDivByK(nums, 5));
 	}
 	
-    public static int subarraysDivByK(int[] A, int K) {
+    public static int subarraysDivByK(int[] nums, int k) {
         Map<Integer, Integer> map = new HashMap<>();
         map.put(0, 1);
         int count = 0, sum = 0;
-        for(int a : A) {
-            sum = (sum + a) % K;
-            if(sum < 0) sum += K;  // Because -1 % 5 = -1, but we need the positive mod 4
+        for(int a : nums) {
+            sum = (sum + a) % k;
+            if(sum < 0) sum += k;  // Because -1 % 5 = -1, but we need the positive mod 4
             count += map.getOrDefault(sum, 0);
             map.put(sum, map.getOrDefault(sum, 0) + 1);
         }
